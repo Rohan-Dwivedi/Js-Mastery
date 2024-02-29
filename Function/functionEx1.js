877f6b6cdd14e606e01f9136f5d4cf542c3e19da
Rohan Kumar Dwivedi <rohandwivedi1203@gmail.com>
2024-02-29 10:41:30 +0530

    Functions() in JS

---
+++ Function/functionEx1.js
@@ -0,0 +1,7 @@
+// write a function to print summ of two numbers;
+
+function sum(a,b){
+    console.log("The Sum of two number is :",a+b);
+}
+
+sum(9,10);
---
+++ Function/functionEx3.js
@@ -0,0 +1,7 @@
+// Arrow Function;  This is used in modern JavaScript
+
+let arrowFunction = (a, b) => {
+    return a+b;
+};
+
+console.log(arrowFunction(9,10));
