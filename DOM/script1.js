a3f912cb238a771a52366029ceef42c6993e03e9
Rohan Kumar Dwivedi <rohandwivedi1203@gmail.com>
2024-03-01 12:32:02 +0530

    Changes

--- DOM/script1.js
+++ DOM/script1.js
@@ -3,10 +3,16 @@ Create 3 div with same class name = box and then access them and add some
 unique text into each box using javascript only. 
 */
 let acc = document.querySelectorAll(".box");
-console.dir(acc);
+//console.dir(acc);
 
-acc[0].innerHTML = "This is first box";
-acc[1].innerHTML = "This is second box";
-acc[2].innerHTML = "This is third box";
+//acc[0].innerHTML = "This is first box";
+//acc[1].innerHTML = "This is second box";
+//acc[2].innerHTML = "This is third box";
 
-console.log(acc[0].innerHTML, acc[1].innerHTML, acc[2].innerHTML);
+//another way using loops 
+
+ind=0
+for (i of acc){
+    i.innerText = `This is ${ind} box`
+    ind++;
+}
